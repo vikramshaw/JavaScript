ea7200d8fc2e62d9a0f7b97ce18fdf4186fabc2f
Vikram Shaw <shawvikram15@gmail.com>
2023-12-26 15:56:33 +0530

    Events Update

--- 3. DOM/Finished/9. 3 ways to add events/app.js
+++ 3. DOM/Finished/9. 3 ways to add events/app.js
@@ -1,21 +1,34 @@
 // ----------- BAD WAY-----------
-const secondBtn = document.querySelector(".second-btn");
-secondBtn.onclick = function () {
-  console.log("Hello World");
-};
+// const secondBtn = document.querySelector(".second-btn");
+// secondBtn.onclick = function () {
+//   console.log("Hello World");
+// };
 
-// ----------- GREAT WAY-----------
-const best = document.querySelector(".best");
+// // ----------- GREAT WAY-----------
+// const best = document.querySelector(".best");
 
-best.addEventListener("click", () => {
-  console.log("Yellow");
-});
+// best.addEventListener("click", () => {
+//   console.log("Yellow");
+// });
 
-// ----------- Event (e) Object -----------
-// It's an event object which contains information about perticular event.
+// // ----------- Event (e) Object -----------
+// // It's an event object which contains information about perticular event.
 
-const para = document.querySelector(".para");
+// const para = document.querySelector(".para");
 
-para.addEventListener("click", (e) => {
-  console.log(e);
+// para.addEventListener("click", (e) => {
+//   console.log(e);
+// });
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   console.log(event);
+// });
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(input.value);
 });
